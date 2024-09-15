67e0709954afe34e9d1fc33f33d5ae17d3fb6b71
Hezrongg <hezrongg26@gmail.com>
2024-09-15 12:29:09 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//CONVERT A STRING TO A NUMBER
+let price = "200";
+let priceParse = parseInt(price);
+console.log(priceParse);
+
+let number = Number(price);
+console.log(number);
+
+let plusSign = +price;
+console.log(plusSign);
+
+//CONVERT A NUMBER TO A STRING
+let distance = 150;
+let theDistance = distance.toString();
+console.log(theDistance);
+let whatDistance = String(distance);
+console.log(whatDistance);
+
+//CONVERT STRING TO A DECIMAL NUMBER
+let weight = "72.5";
+let weightParse = parseFloat(weight);
+console.log(weightParse);
